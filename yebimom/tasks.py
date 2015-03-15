e97d48bcea6ccc6a7d6fa66b5c5abe7116ea63fb
dobestan <dobestan@gmail.com>
2015-03-15 23:52:16 +0900

    refactoring email sending process

--- yebimom/tasks.py
+++ yebimom/tasks.py
@@ -17,10 +17,20 @@ def send_contact_email(email, title, content):
         'content': content
     })
 
+    # send email to customer
     send_mail(
         "[예비맘닷컴] 1:1 문의가 등록되었습니다.",
         email_template.render(email_context),
         "예비맘 <contact@yebimom.com>",
-        [email, "contact@yebimom.com"],
+        [email],
+        fail_silently=False,
+    )
+
+    # send email to admin
+    send_mail(
+        "[예비맘닷컴] 1:1 문의가 등록되었습니다. - " + title.encode('utf-8'),
+        content,
+        "예비맘 <contact@yebimom.com>",
+        ["contact@yebimom.com"],
         fail_silently=False,
     )
