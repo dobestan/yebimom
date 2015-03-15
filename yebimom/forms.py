d7f57e7fae79a14f0ee8503821008afea753c1ca
dobestan <dobestan@gmail.com>
2015-03-15 22:51:09 +0900

    install django-mailgun package to change django email backend

--- yebimom/forms.py
+++ yebimom/forms.py
@@ -4,7 +4,7 @@ from django_summernote.widgets import SummernoteWidget
 
 
 class ContactForm(forms.Form):
-    title = forms.CharField(max_length = 255)
+    title = forms.CharField(max_length=255)
     content = forms.CharField(widget=SummernoteWidget())
 
     email = forms.EmailField()
